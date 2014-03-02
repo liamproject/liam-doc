07d54444568fd3c7f4d7515d9ab7ecda0c7b93f4
Eric Lease Morgan <eric_morgan@infomotions.com>
2014-03-02 18:37:44 -0500

    Started marking sections as "done"

--- make-book.sh
+++ make-book.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+cd "/Users/eric/Documents/linked data guidebook/liam-doc/"
+mv guidebook.txt x-guidebook.txt
 FILES=./guidebook/*.txt
 for f in $FILES
 do
-  cat $f
-  printf "\f"
+  cat $f >> guidebook.txt
+  printf "\f" >> guidebook.txt
 done
